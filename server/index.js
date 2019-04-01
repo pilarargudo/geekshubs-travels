3c719599ebf5004f3d7ff7e94d2c848df171fde2
Pilar Argudo <pargudo@disid.com>
2019-04-01 14:10:03 +0200

    Añadidas secciones quiénes somos y contacto

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
       {slogan: '¡Viaja!', subslogan: 'Descubre lugares maravillosos', 
       button: 'Ver destinos', linkButton:'#travels', 
       imgBanner: '',}
-    ],
+    ],    
     imgBackground: 'nathan-anderson-316188-unsplash.jpg',
     travels: [
       {id: 1, img: 'beijing.jpg', city: 'Beijing', discount: '90', price: '200'},
@@ -56,6 +56,21 @@ app.get('/', (req, res) => {
       {id: 4, img: 'ciudad_mexico.jpg', city: 'Ciudad de México', discount: '115', price:'450' },
       {id: 5, img: 'new_york.jpg', city: 'New York', discount: '65', price:'350' },
       {id: 6, img: 'tokyo.jpg', city: 'Tokyo', discount: '90', price:'320' },
+    ],
+    who: [
+      {
+        title: 'Quiénes somos',
+        text: 'GeeksHubs Travels es una agencia de de viajes online creada en 2009 por un equipo de viajeros entusiastas que deciden hacer de su pasión por conocer el mundo un modo de vida. Desde nuestros inicios hemos diseñado más de 500 viajes organizados a diferentes destinos de Europa, Norteamérica y Asia. Nuestro objetivo ahora es ir ampliando horizontes.', 
+        button: 'Descubrir', 
+        linkButton:'#', 
+        imgBanner: 'travel_3.jpg'
+      }
+    ],
+    contact: [
+      {
+        title: 'Contacta con nosotros',
+        text: 'texto texto texto', 
+      }
     ],
     layout: 'template',
   });
