2084b26fa9bd753d36fa5742f57a2cdc954f535a
Pilar Argudo <pargudo@disid.com>
2019-04-05 14:12:01 +0200

    Fixed uploads dir

--- server/config/multer.js
+++ server/config/multer.js
@@ -3,7 +3,8 @@ const Multer = require( 'multer' );
 const options = {
     storage: Multer.diskStorage( {
         destination: ( req, file, callback ) => {
-            callback( null, './uploads/' );
+            callback(null, 'server/uploads');
+
         },
         filename: ( req, file, callback ) => {
             callback( null, file.originalname );
